a02b22705cc88ebadcfe65becfa8293487b38cdc
xoff69 <xoff.pichon@gmail.com>
2024-09-23 14:59:13 -0600

    with tab

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   plugins: [
     vue(),
   ],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
