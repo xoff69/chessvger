fe9eaef68d73a6b8468c689c19accc9454ed9089
xoff69 <xoff.pichon@gmail.com>
2024-11-22 14:26:16 -0700

    ajout redis

--- server/queues/src/main/java/com/xoff/chessvger/queues/Main.java
+++ server/queues/src/main/java/com/xoff/chessvger/queues/Main.java
@@ -1,8 +1,15 @@
 package com.xoff.chessvger.queues;
 
+import redis.clients.jedis.Jedis;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("hello");
+
+    Jedis jedis = new Jedis();
+    jedis.set("events/city/rome", "32,15,223,828");
+    String cachedResponse = jedis.get("events/city/rome");
+    System.out.println("hello"+cachedResponse);
   }
 
 }
