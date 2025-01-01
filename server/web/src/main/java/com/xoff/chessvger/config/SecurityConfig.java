c550f4918dff46bac2600b20fd17b99e1b9464e7
xoff69 <xoff.pichon@gmail.com>
2025-01-01 09:44:44 -0700

    update sql

--- server/web/src/main/java/com/xoff/chessvger/config/SecurityConfig.java
+++ server/web/src/main/java/com/xoff/chessvger/config/SecurityConfig.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class SecurityConfig {
@@ -40,5 +42,46 @@ private CustomUserDetailsService userDetailsService;
         .and()
         .httpBasic();
     return http.build();
+  }
+
+  @Bean
+  SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http,
+                                                 HandlerMappingIntrospector introspector)
+      throws Exception {
+
+    //   MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+    // http.authorizeHttpRequests((requests) -> requests.requestMatchers(mvcMatcherBuilder.
+    // pattern("/bds")).permitAll().requestMatchers(mvcMatcherBuilder.pattern("/features")).
+    // permitAll().anyRequest().authenticated()).formLogin(Customizer.withDefaults()).
+    // httpBasic(Customizer.withDefaults());
+   /*     http.authorizeHttpRequests((requests)
+                -> requests.requestMatchers(mvcMatcherBuilder.pattern("/bds")).permitAll().
+                requestMatchers(mvcMatcherBuilder.pattern("/features")).permitAll().
+                anyRequest().authenticated()).formLogin(Customizer.withDefaults()).
+                httpBasic(Customizer.withDefaults());
+    */
+    // FIXME
+    // le srf permet d autoriser le post sur les tests
+    http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+    return http.build();
+
+    /*
+     *  Configuration to deny all the requests
+     */
+    /*http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
+            .formLogin(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults());
+    return http.build();*/
+
+    /*
+     *  Configuration to permit all the requests
+     */
+    /*
+    http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+            .formLogin(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults());
+    return http.build();
+    */
   }
 }
--- server/web/src/main/java/com/xoff/chessvger/config/WebSocketConfig.java
+++ server/web/src/main/java/com/xoff/chessvger/config/WebSocketConfig.java
@@ -1,25 +1,22 @@
 package com.xoff.chessvger.config;
 
+import com.xoff.chessvger.ui.web.controller.MessageToClientHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
   @Override
-  public void configureMessageBroker(MessageBrokerRegistry registry) {
-    // Configure un broker simple pour gérer les topics
-    registry.enableSimpleBroker("/topic");
-    registry.setApplicationDestinationPrefixes("/app");
-  }
-
-  @Override
-  public void registerStompEndpoints(StompEndpointRegistry registry) {
-    // Point d'entrée WebSocket accessible depuis le frontend
-    registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(new MessageToClientHandler(), "/ws/messageToClient").setAllowedOrigins("*");
   }
 }
