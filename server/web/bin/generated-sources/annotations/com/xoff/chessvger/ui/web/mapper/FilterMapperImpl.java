f7e590617240cb48abdb66fd01106c8d31bc8d1e
xoff69 <xoff.pichon@gmail.com>
2025-01-23 21:36:48 -0700

    fix bug npm dockerfile client

--- server/web/bin/generated-sources/annotations/com/xoff/chessvger/ui/web/mapper/FilterMapperImpl.java
+++ server/web/bin/generated-sources/annotations/com/xoff/chessvger/ui/web/mapper/FilterMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-01-22T21:01:34-0700",
+    date = "2025-01-23T20:58:43-0700",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.41.0.z20250115-2156, environment: Java 21.0.5 (Eclipse Adoptium)"
 )
 @Component
